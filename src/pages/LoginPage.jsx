e996b55e1afec7d6ebfdea629decba5f22778412
Oscar Monroy <oscarmonroy657@gmail.com>
2022-10-02 17:36:05 -0600

    The style for the login form is ready.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,78 @@
-import React from "react";
+import { Button, Divider, Form, Input, InputNumber, Typography } from "antd";
+import { SaveOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+const { Title, Text } = Typography;
 
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const navigate = useNavigate();
+  const onFinish = (values) => {
+    console.log("Success:", values);
+    navigate("/desk");
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <>
+      <Title level={2}>Login</Title>
+      <Text>Enter your name and your number desk</Text>
+      <Divider />
+
+      <Form
+        name="basic"
+        labelCol={{
+          span: 4,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Desk number"
+          name="desknumber"
+          rules={[
+            {
+              required: true,
+              message: "Please input your desk number!",
+            },
+          ]}
+        >
+          <InputNumber min={1} max={99} />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 4,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit" shape="round">
+            <SaveOutlined />
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
 };
--- src/pages/RouterPage.jsx
+++ src/pages/RouterPage.jsx
@@ -22,7 +22,7 @@ export const RouterPage = () => {
   return (
     <Router>
       <Layout style={{ minHeight: "100vh" }}>
-        <Sider trigger={null}>
+        <Sider trigger={null} collapsedWidth="0" breakpoint="md">
           <div className="logo" />
           <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
             <Menu.Item key={"1"} icon={<UserOutlined />}>
