ed899ca66ff7a9a2647007fac31fdd327a9ff092
Oscar Monroy <oscarmonroy657@gmail.com>
2022-10-15 16:48:25 -0600

    Desing is done.

--- src/pages/CreateTicketPage.jsx
+++ src/pages/CreateTicketPage.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import { Button, Col, Row, Typography } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
 export const CreateTicketPage = () => {
-  return <div>CreateTicketPage</div>;
+  const createTicket = () => {
+    console.log("Creating...");
+  };
+  return (
+    <>
+      <Row>
+        <Col span={14} offset={6} align="center">
+          <Typography.Title level={3}>
+            Press the button to create a ticket
+          </Typography.Title>
+          <Button
+            type="primary"
+            shape="round"
+            icon={DownloadOutlined}
+            size="large"
+            onClick={createTicket}
+          >
+            New ticket
+          </Button>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: 100 }}>
+        <Col span={14} offset={6} align="center">
+          <Typography.Text level={2}>Their number</Typography.Text>
+          <Typography.Text type="success" style={{ fontSize: 55 }}>
+            55
+          </Typography.Text>
+        </Col>
+      </Row>
+    </>
+  );
 };
--- src/pages/LinePage.jsx
+++ src/pages/LinePage.jsx
@@ -1,5 +1,54 @@
+import { Card, Col, Divider, List, Row, Tag, Typography } from "antd";
 import React from "react";
+import { lineDummy } from "../mocks/lineDummt";
 
 export const LinePage = () => {
-  return <div>LinePage</div>;
+  return (
+    <>
+      <Typography.Title level={1}>Atending the customer</Typography.Title>
+      <Row>
+        <Col span={2}>
+          <List
+            dataSource={lineDummy.slice(0, 3)}
+            renderItem={(item) => (
+              <List.Item>
+                <Card
+                  style={{ width: 300, marginTop: 16 }}
+                  actions={[
+                    <Tag color="volcano">{item.agente}</Tag>,
+                    <Tag color="magenta">Desk: {item.escritorio}</Tag>,
+                  ]}
+                >
+                  <Typography.Title>No. {item.ticketNo}</Typography.Title>
+                </Card>
+              </List.Item>
+            )}
+          />
+        </Col>
+        <Col span={2}>
+          <Divider>History</Divider>
+          <List
+            dataSource={lineDummy.slice(3)}
+            renderItem={(item) => (
+              <List.Item>
+                <List.Item.Meta
+                  title={`Ticket No. ${item.ticketNo}`}
+                  description={
+                    <>
+                      <Typography.Text type="secondary">Desk: </Typography.Text>
+                      <Tag color="magenta">{item.ticketNo}</Tag>
+                      <Typography.Text type="secondary">
+                        Agent:{" "}
+                      </Typography.Text>
+                      <Tag color="volcano">{item.agente}</Tag>
+                    </>
+                  }
+                />
+              </List.Item>
+            )}
+          />
+        </Col>
+      </Row>
+    </>
+  );
 };
